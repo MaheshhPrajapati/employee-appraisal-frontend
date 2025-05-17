655ae02db3b8201ca47962344ffc0810b4a0e952
MaheshhPrajapati <59406642+MaheshhPrajapati@users.noreply.github.com>
2025-05-17 10:23:47 +0530

    Update DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -20,15 +20,20 @@ const DashboardPage: React.FC = () => {
       .catch(err => console.error('Error fetching user:', err));
   }, []);
   return (
-    <div>
+     <div className="dashboard-container">
       <nav className="dashboard-navbar">
         <span className="dashboard-title">Employee Appraisal System</span>
         <span className="dashboard-user">Welcome, {user?.name || 'Loading...'}</span>
       </nav>
+      
       <button className="appraisal-button" onClick={() => { navigate('/backend/employeeDashboard') }}>+ Fill Appraisal Form</button>
+
       <div className="dashboard-content">
-        <h2>Dashboard</h2>
-        {/* <p>This is your secure area.</p> */}
+        <h2 className="dashboard-heading">Dashboard</h2>
+        <div className="dashboard-user-info">
+          <h3>User Info:</h3>
+          <p>Email: {user?.email}</p>
+        </div>
       </div>
     </div>
   );
